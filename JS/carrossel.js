3f8a4aa109ed2d3a266bbd3a9b65c2d25b3bbe5f
Jorge <jorgejapao00@gmail.com>
2025-02-27 15:08:59 -0300

    Carousel remade. Animation added.

--- JS/carrossel.js
+++ JS/carrossel.js
@@ -1,16 +1,28 @@
 const images = document.querySelectorAll('.carousel-images img');
+const carousel = document.querySelector('.carousel-images');
 let currentIndex = 0;
 
 function showImage(index) {
-  images.forEach((img, i) => {
-    img.style.display = i === index ? 'block' : 'none';
-  });
+  const offset = -index * 100; 
+  carousel.style.transform = `translateX(${offset}%)`; //
 }
 
 function nextImage() {
-  currentIndex = (currentIndex + 1) % images.length;
-  showImage(currentIndex);
+  currentIndex = (currentIndex + 1) % images.length; // Avança para a próxima imagem
+  
+
+  if (currentIndex === 0) {
+    carousel.style.transition = 'none'; 
+    showImage(currentIndex);
+
+   
+    setTimeout(() => {
+      carousel.style.transition = 'transform 1s ease-in-out';
+    }, 20); 
+  } else {
+    showImage(currentIndex); 
+  }
 }
 
-showImage(currentIndex);
+showImage(currentIndex); 
 setInterval(nextImage, 3000);
